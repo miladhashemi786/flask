c05254c85a61b9d06605a099a404ae137bd23bce
miladhashemi786 <150113259+miladhashemi786@users.noreply.github.com>
2023-11-18 15:22:28 -0800

    Update typing_app_decorators.py

--- tests/typing/typing_app_decorators.py
+++ tests/typing/typing_app_decorators.py
@@ -1,36 +1,10 @@
-from __future__ import annotations
-
 from flask import Flask
-from flask import Response
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Hello, this is your Flask web app!'
 
-@app.after_request
-def after_sync(response: Response) -> Response:
-    return Response()
-
-
-@app.after_request
-async def after_async(response: Response) -> Response:
-    return Response()
-
-
-@app.before_request
-def before_sync() -> None:
-    ...
-
-
-@app.before_request
-async def before_async() -> None:
-    ...
-
-
-@app.teardown_appcontext
-def teardown_sync(exc: BaseException | None) -> None:
-    ...
-
-
-@app.teardown_appcontext
-async def teardown_async(exc: BaseException | None) -> None:
-    ...
+if __name__ == '__main__':
+    app.run(debug=True)
